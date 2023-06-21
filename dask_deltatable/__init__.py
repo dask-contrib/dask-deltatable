303295701cbd3510b7dca4178d77ec88504cae16
Thomas Grainger <tagrain@gmail.com>
2023-06-21 12:02:32 +0100

    delete pyi files, types are already inlined

--- dask_deltatable/__init__.py
+++ dask_deltatable/__init__.py
@@ -1,1 +1,3 @@
-from .core import read_delta_history, read_delta_table, vacuum
+from .core import read_delta_history as read_delta_history
+from .core import read_delta_table as read_delta_table
+from .core import vacuum as vacuum
