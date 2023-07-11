69555cdf933df569074cf80fe46da77880d006ca
Florian Jetter <fjetter@users.noreply.github.com>
2023-07-11 13:18:46 +0200

    Drop python 3.8 (#35)

--- dask_deltatable/write.py
+++ dask_deltatable/write.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 import json
 import uuid
+from collections.abc import Mapping
 from datetime import datetime
 from pathlib import Path
-from typing import Any, Literal, Mapping
+from typing import Any, Literal
 
 import dask.dataframe as dd
 import pyarrow as pa
